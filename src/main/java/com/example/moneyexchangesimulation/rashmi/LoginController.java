fb8727f801ed96e73703848152533b1d5d135719
rashmii01 <2220871@iub.edu.bd>
2025-04-09 08:56:31 +0600

    student Dashboard fxml

--- src/main/java/com/example/moneyexchangesimulation/rashmi/LoginController.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/LoginController.java
@@ -1,7 +1,25 @@
 package com.example.moneyexchangesimulation.rashmi;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController
 {
     @javafx.fxml.FXML
+    private TextField userNameTextField;
+    @javafx.fxml.FXML
+    private PasswordField passwordField;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+
+    }
+
+    @javafx.fxml.FXML
+    public void loginButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/StudentDashboardController.java
@@ -0,0 +1,7 @@
+package com.example.moneyexchangesimulation.rashmi;
+
+public class StudentDashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
