d2ed3d39d08be8cd973d730aa86679c2eb9e09ba
Israt_Jahan_Liya <isratjahanliya28@gmail.com>
2025-04-11 16:56:09 +0600

    i have created fxml files

---
+++ src/main/resources/com/example/moneyexchangesimulation/Israt Jahan Liya/LoginController.java
@@ -0,0 +1,19 @@
+public class LoginController
+{
+    @javafx.fxml.FXML
+    private TextField passwordid;
+    @javafx.fxml.FXML
+    private Label nametextfeild;
+    @javafx.fxml.FXML
+    private Label passwordtextfeild;
+    @javafx.fxml.FXML
+    private TextField nameid;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void loginonaction(ActionEvent actionEvent) {
+    }
+}
