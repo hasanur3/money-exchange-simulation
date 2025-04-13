7225d8043dc02d6c55f0010b84a87672dd752204
hasanur3 <2330951@iub.edu.bd>
2025-04-14 00:48:57 +0600

    cashier Goal-1 fxml file added

---
+++ src/main/java/com/example/moneyexchangesimulation/Hasan/Cashier_dashboardController.java
@@ -0,0 +1,46 @@
+package com.example.moneyexchangesimulation.Hasan;
+
+import javafx.event.ActionEvent;
+
+public class Cashier_dashboardController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyId(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cashBalance(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void txnApproval(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void exchangeRates(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void receipt(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logs(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void processExchange(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void refunds(ActionEvent actionEvent) {
+    }
+}
