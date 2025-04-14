0186819c9ff09d5b0eb7bac8f68fdc6f0bb04c4b
hasanur3 <2330951@iub.edu.bd>
2025-04-14 18:17:43 +0600

    scene switching activated

--- src/main/java/com/example/moneyexchangesimulation/Hasan/Cashier_dashboardController.java
+++ src/main/java/com/example/moneyexchangesimulation/Hasan/Cashier_dashboardController.java
@@ -12,38 +12,46 @@ public class Cashier_dashboardController
 
     @javafx.fxml.FXML
     public void verifyId(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("cashier_Goal-2");
+    }
+
+    @javafx.fxml.FXML
+    public void logOutButton(ActionEvent actionEvent) throws IOException {
+
+    }
+
+    @javafx.fxml.FXML
+    public void cashBalance(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("cashier_Goal-4");
+    }
+
+    @javafx.fxml.FXML
+    public void txnApproval(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("cashier_Goal-7");
+    }
+
+    @javafx.fxml.FXML
+    public void exchangeRates(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("cashier_Goal-6");
+    }
+
+    @javafx.fxml.FXML
+    public void receipt(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("cashier_Goal-5");
+    }
+
+    @javafx.fxml.FXML
+    public void logs(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("cashier_Goal-8");
+    }
+
+    @javafx.fxml.FXML
+    public void processExchange(ActionEvent actionEvent) throws IOException{
         SceneSwitcher.switchTo("cashier_Goal1");
     }
 
     @javafx.fxml.FXML
-    public void logOutButton(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void cashBalance(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void txnApproval(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void exchangeRates(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void receipt(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void logs(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void processExchange(ActionEvent actionEvent) {
-    }
-
-    @javafx.fxml.FXML
-    public void refunds(ActionEvent actionEvent) {
+    public void refunds(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("cashier_Goal-3");
     }
 }
