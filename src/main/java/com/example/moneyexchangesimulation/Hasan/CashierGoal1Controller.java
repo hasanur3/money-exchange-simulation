2cd5148cec2bda6eaf0fe1d4652457f711b5fad6
hasanur3 <2330951@iub.edu.bd>
2025-04-14 22:18:14 +0600

    cashier goal-1 scene design completed

--- src/main/java/com/example/moneyexchangesimulation/Hasan/CashierGoal1Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/Hasan/CashierGoal1Controller.java
@@ -1,7 +1,51 @@
 package com.example.moneyexchangesimulation.Hasan;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class CashierGoal1Controller
 {
     @javafx.fxml.FXML
+    private TextField customerNameField;
+    @javafx.fxml.FXML
+    private TextField customerIdField;
+    @javafx.fxml.FXML
+    private TextField amountField;
+    @javafx.fxml.FXML
+    private ComboBox currencyTypeCombo;
+    @javafx.fxml.FXML
+    private AnchorPane validationLabel;
+    @javafx.fxml.FXML
+    private Label convertedAmountLabel;
+    @javafx.fxml.FXML
+    private Label transactionStatusLabel;
+    @javafx.fxml.FXML
+    private Label receiptStatusLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void validateFunds(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleNewTransaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmTransaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void calculateExchange(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendReceipt(ActionEvent actionEvent) {
+    }
+}
