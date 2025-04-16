514011447a1e9bc016a7fa5909e991928ef75501
rashmii01 <2220871@iub.edu.bd>
2025-04-16 14:15:56 +0600

    student goal 6 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal6Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal6Controller.java
@@ -3,5 +3,31 @@ package com.example.moneyexchangesimulation.rashmi.Controller;
 public class StudentGoal6Controller
 {
     @javafx.fxml.FXML
+    private Tab paymentDetailsTab;
+    @javafx.fxml.FXML
+    private TextField accNumberTextField;
+    @javafx.fxml.FXML
+    private Tab paymentMethodTab;
+    @javafx.fxml.FXML
+    private TabPane paymentTabpane;
+    @javafx.fxml.FXML
+    private TextField cardDetailsTextField;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void savePayment(ActionEvent actionEvent) {
+    }
+}
