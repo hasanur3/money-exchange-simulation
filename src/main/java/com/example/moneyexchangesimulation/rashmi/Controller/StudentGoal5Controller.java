5a2590f43b0820533e078c5f3f90d52531228dd5
rashmii01 <2220871@iub.edu.bd>
2025-04-16 13:49:13 +0600

    student goal 5 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal5Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal5Controller.java
@@ -3,5 +3,37 @@ package com.example.moneyexchangesimulation.rashmi.Controller;
 public class StudentGoal5Controller
 {
     @javafx.fxml.FXML
+    private TextField enterAmount;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextArea receiptTextArea;
+    @javafx.fxml.FXML
+    private Label balanceLabel;
+    @javafx.fxml.FXML
+    private ComboBox currencyComBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void exchangeNow(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmTransaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void downloadReceipt(ActionEvent actionEvent) {
+    }
+}
