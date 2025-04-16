fd9433ac327b34967089d511e0d86ecc16dc049e
rashmii01 <2220871@iub.edu.bd>
2025-04-16 11:37:22 +0600

    StudentGoal 4 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal4Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal4Controller.java
@@ -3,5 +3,39 @@ package com.example.moneyexchangesimulation.rashmi.Controller;
 public class StudentGoal4Controller
 {
     @javafx.fxml.FXML
+    private TextField referCodeTextField;
+    @javafx.fxml.FXML
+    private Label bonusLabel;
+    @javafx.fxml.FXML
+    private Label showCode;
+    @javafx.fxml.FXML
+    private Label messageLabel;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void earnRewards(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void referProgram(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void redeemRewards(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void share(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+}
