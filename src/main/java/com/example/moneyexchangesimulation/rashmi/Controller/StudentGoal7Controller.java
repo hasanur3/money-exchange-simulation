81a7c1c6213752315a36d9e9bcfad08a043fe6eb
rashmii01 <2220871@iub.edu.bd>
2025-04-16 15:01:21 +0600

    student goal 7 and 8 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal7Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal7Controller.java
@@ -3,5 +3,37 @@ package com.example.moneyexchangesimulation.rashmi.Controller;
 public class StudentGoal7Controller
 {
     @javafx.fxml.FXML
+    private TextField emailTextField;
+    @javafx.fxml.FXML
+    private TextField nameTextFiled;
+    @javafx.fxml.FXML
+    private TextField accNumTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void transferMoney(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sendSMS(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void validateTrans(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void confirmTrans(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal8Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal8Controller.java
@@ -1,7 +1,33 @@
 package com.example.moneyexchangesimulation.rashmi.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+
 public class StudentGoal8Controller
 {
     @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void applyDis(ActionEvent actionEvent) {
+    }
+
+
+
+    @javafx.fxml.FXML
+    public void stuDis(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void verifyStuStatus(ActionEvent actionEvent) {
+    }
+}
