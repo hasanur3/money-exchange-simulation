e802b58b074ad0d40c06e77094d4f68546e46003
rashmii01 <2220871@iub.edu.bd>
2025-04-17 08:14:34 +0600

    modification

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal8Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal8Controller.java
@@ -18,16 +18,14 @@ public class StudentGoal8Controller
 
     @javafx.fxml.FXML
     public void applyDis(ActionEvent actionEvent) {
-        disRateLabel.setText("Student Discount: 10% Applied");
-        showAlert(Alert.AlertType.INFORMATION, "Discount Applied", "Your 10% student discount has been applied successfully.");
+
     }
 
 
 
     @javafx.fxml.FXML
     public void stuDis(ActionEvent actionEvent) {
-        disRateLabel.setText("Available Discount: 10% for verified students");
-        showAlert(Alert.AlertType.INFORMATION, "Discount Info", "Eligible students receive a 10% discount on all transactions.");
+
     }
 
     @javafx.fxml.FXML
@@ -42,7 +40,6 @@ public class StudentGoal8Controller
 
     @javafx.fxml.FXML
     public void verifyStuStatus(ActionEvent actionEvent) {
-        disRateLabel.setText("Student Status: Verified");
-        showAlert(Alert.AlertType.INFORMATION, "Status Verified", "Your student status has been successfully verified.");
+
     }
 }
