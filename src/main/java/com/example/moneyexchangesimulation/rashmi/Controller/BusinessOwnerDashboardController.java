0d74794c8017a82faa84f30e7321e6f66dd67103
rashmii01 <2220871@iub.edu.bd>
2025-04-17 08:42:31 +0600

    Business Owner Dashboard implementation

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerDashboardController.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerDashboardController.java
@@ -1,6 +1,9 @@
 package com.example.moneyexchangesimulation.rashmi.Controller;
 
+import com.example.moneyexchangesimulation.Hasan.SceneSwitcher;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 public class BusinessOwnerDashboardController
 {
@@ -8,39 +11,50 @@ public class BusinessOwnerDashboardController
     public void initialize() {
     }
 
+
+
     @javafx.fxml.FXML
-    public void trackTotalExpensesButton(ActionEvent actionEvent) {
+    public void logoutButton(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal1");
     }
 
     @javafx.fxml.FXML
-    public void handleEmployeeButton(ActionEvent actionEvent) {
+    public void genBusinessFinReports(ActionEvent actionEvent) throws IOException{
+        SceneSwitcher.switchTo("BusinessOwnerGoal5");
     }
 
     @javafx.fxml.FXML
-    public void manageBusinessExpensesButton(ActionEvent actionEvent) {
+    public void manageBusinessExpenses(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal6");
     }
 
     @javafx.fxml.FXML
-    public void exportDataExpensesButton(ActionEvent actionEvent) {
+    public void monitorBusinessExchange(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal1");
     }
 
     @javafx.fxml.FXML
-    public void monitorBusinessExchangeButton(ActionEvent actionEvent) {
+    public void accessBusinessLoanOffer(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal2");
     }
 
     @javafx.fxml.FXML
-    public void logoutButton(ActionEvent actionEvent) {
+    public void manageSupplierPayments(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal8");
     }
 
     @javafx.fxml.FXML
-    public void offerBusinessLoanButton(ActionEvent actionEvent) {
+    public void handleEmployeePayroll(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal7");
     }
 
     @javafx.fxml.FXML
-    public void genExpensesReportButton(ActionEvent actionEvent) {
+    public void integrateBusinessBankAcc(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal3");
     }
 
     @javafx.fxml.FXML
-    public void genBusinessFinRepButton(ActionEvent actionEvent) {
+    public void automateBulkTrans(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchTo("BusinessOwnerGoal4");
     }
 }
