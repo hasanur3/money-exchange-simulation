6a5a07db56afb8468c952dec668736eed5abd27e
hasanur3 <2330951@iub.edu.bd>
2025-04-18 15:42:24 +0600

    system Manager goal-6 complete

--- src/main/java/com/example/moneyexchangesimulation/Hasan/SystemManagerGoal6Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/Hasan/SystemManagerGoal6Controller.java
@@ -1,7 +1,44 @@
 package com.example.moneyexchangesimulation.Hasan;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SystemManagerGoal6Controller
 {
     @javafx.fxml.FXML
+    private Label otpLabel;
+    @javafx.fxml.FXML
+    private Label statusLabel;
+    @javafx.fxml.FXML
+    private TextField otpField;
+
+
+    private String generatedOtp;
+
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void enable2FA(ActionEvent actionEvent) {
+        generatedOtp = "123456";
+        otpLabel.setText("📨 OTP sent to registered email/phone: " + generatedOtp);
+        statusLabel.setText("");
+    }
+
+    @javafx.fxml.FXML
+    public void validateOTP(ActionEvent actionEvent) {
+        String enteredOtp =otpField.getText();
+        if (enteredOtp.equals(generatedOtp)) {
+            statusLabel.setText("✅ 2FA enabled successfully.");
+        } else {
+            statusLabel.setText("❌ Incorrect OTP. Please try again.");
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void goToDashboard(ActionEvent actionEvent) {
+    }
+}
