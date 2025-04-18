a9e6abc32e30768ecb2bf36eec224f5c5f1ba1ba
rashmii01 <2220871@iub.edu.bd>
2025-04-18 11:51:12 +0600

    business owner goal 4 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal4Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal4Controller.java
@@ -1,7 +1,25 @@
 package com.example.moneyexchangesimulation.rashmi.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class BusinessOwnerGoal4Controller
 {
     @javafx.fxml.FXML
+    private Label statusShowLabel;
+    @javafx.fxml.FXML
+    private Label validationLabel;
+    @javafx.fxml.FXML
+    private Label confirmationLabel;
+    @javafx.fxml.FXML
+    private TextField bankAccountNumberTextField;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void bulkTransaction(ActionEvent actionEvent) {
+    }
+}
