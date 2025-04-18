6588c0a88e87cd963e3558a781ce2b58d7bdeed1
rashmii01 <2220871@iub.edu.bd>
2025-04-18 11:49:39 +0600

    business owner goal 2 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal2Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal2Controller.java
@@ -1,7 +1,46 @@
 package com.example.moneyexchangesimulation.rashmi.Controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class BusinessOwnerGoal2Controller
 {
     @javafx.fxml.FXML
+    private TableView loanTableView;
+    @javafx.fxml.FXML
+    private Label loanAppliedOrNot;
+    @javafx.fxml.FXML
+    private TableColumn transactionIDTableColumn;
+    @javafx.fxml.FXML
+    private TextField totalTransAmountTextField;
+    @javafx.fxml.FXML
+    private TableColumn availableLoanTableColumn;
+    @javafx.fxml.FXML
+    private TextField transAmountForApplyLoanTextField;
+    @javafx.fxml.FXML
+    private Label eligibleForLoanOrNot;
+    @javafx.fxml.FXML
+    private ComboBox currencyTypeComboBox;
+    @javafx.fxml.FXML
+    private TableColumn transactionAmountTableColumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void applyForALoan(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void businessLoan(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+}
