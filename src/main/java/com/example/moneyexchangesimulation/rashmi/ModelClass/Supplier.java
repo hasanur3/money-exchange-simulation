68c79f19c06beadb13bd4723ce5b541245b83860
rashmii01 <2220871@iub.edu.bd>
2025-04-19 00:06:25 +0600

    supplier model class modify

--- src/main/java/com/example/moneyexchangesimulation/rashmi/ModelClass/Supplier.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/ModelClass/Supplier.java
@@ -1,15 +1,9 @@
 package com.example.moneyexchangesimulation.rashmi.ModelClass;
 public class Supplier {
-    private int accountNumber;
-    private String name,cardDetails,transactionID,paymentDetails;
 
-    public int getAccountNumber() {
-        return accountNumber;
-    }
+    private String name;
 
-    public void setAccountNumber(int accountNumber) {
-        this.accountNumber = accountNumber;
-    }
+
 
     public String getName() {
         return name;
@@ -19,47 +13,21 @@ public class Supplier {
         this.name = name;
     }
 
-    public String getCardDetails() {
-        return cardDetails;
-    }
 
-    public void setCardDetails(String cardDetails) {
-        this.cardDetails = cardDetails;
-    }
-
-    public String getTransactionID() {
-        return transactionID;
-    }
-
-    public void setTransactionID(String transactionID) {
-        this.transactionID = transactionID;
-    }
-
-    public String getPaymentDetails() {
-        return paymentDetails;
-    }
-
-    public void setPaymentDetails(String paymentDetails) {
-        this.paymentDetails = paymentDetails;
-    }
 
     @Override
     public String toString() {
         return "Supplier{" +
-                "accountNumber=" + accountNumber +
+
                 ", name='" + name + '\'' +
-                ", cardDetails='" + cardDetails + '\'' +
-                ", transactionID='" + transactionID + '\'' +
-                ", paymentDetails='" + paymentDetails + '\'' +
+
                 '}';
     }
 
-    public Supplier(int accountNumber, String name, String cardDetails, String transactionID, String paymentDetails) {
-        this.accountNumber = accountNumber;
+    public Supplier(int accountNumber, String name) {
+
         this.name = name;
-        this.cardDetails = cardDetails;
-        this.transactionID = transactionID;
-        this.paymentDetails = paymentDetails;
+
 
 
     }
