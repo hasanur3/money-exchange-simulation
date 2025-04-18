5aebfb7d7457a68ba30201986682e5370b452be0
rashmii01 <2220871@iub.edu.bd>
2025-04-18 09:45:33 +0600

    business owner goal 1 fxml design

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal1Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/BusinessOwnerGoal1Controller.java
@@ -3,5 +3,37 @@ package com.example.moneyexchangesimulation.rashmi.Controller;
 public class BusinessOwnerGoal1Controller
 {
     @javafx.fxml.FXML
+    private TableColumn amountTableColumn;
+    @javafx.fxml.FXML
+    private TableView transactionTableView;
+    @javafx.fxml.FXML
+    private TableColumn currencyTypeTableColumn;
+    @javafx.fxml.FXML
+    private TextField amountTextField;
+    @javafx.fxml.FXML
+    private DatePicker datePicker;
+    @javafx.fxml.FXML
+    private TableColumn dateTableColumn;
+    @javafx.fxml.FXML
+    private ComboBox currencyTypeComboBox;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void download(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void businessTransaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logOut(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void dashboard(ActionEvent actionEvent) {
+    }
+}
