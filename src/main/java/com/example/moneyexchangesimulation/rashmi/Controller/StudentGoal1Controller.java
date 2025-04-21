d482ce3e28ed4d2bfe506fd6dc72fa2cc620bd10
rashmii01 <2220871@iub.edu.bd>
2025-04-21 23:23:14 +0600

    modification

--- src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal1Controller.java
+++ src/main/java/com/example/moneyexchangesimulation/rashmi/Controller/StudentGoal1Controller.java
@@ -16,22 +16,34 @@ public class StudentGoal1Controller
     @javafx.fxml.FXML
     private ComboBox<String> currencyTypeComboBox;
 
-    private final ObservableList<String> currencyList = FXCollections.observableArrayList("USD", "EUR", "JPY", "INR");
+
 
     @javafx.fxml.FXML
     public void initialize() {
-        currencyTypeComboBox.setItems(currencyList);
+
+        currencyTypeComboBox.getItems().addAll("USD", "EUR", "JPY", "INR");
     }
 
     @javafx.fxml.FXML
     public void exchangeRates(ActionEvent actionEvent) {
         String selectedCurrency = currencyTypeComboBox.getValue();
 
-        if (selectedCurrency != null) {
-            int rate = Integer.parseInt(selectedCurrency);
-            showRatesLabel.setText("1 USD = " + rate +" " + selectedCurrency);
-        } else {
+        if (selectedCurrency == null) {
             showRatesLabel.setText("Please select a currency.");
+            return;
+        }
+
+
+        if (selectedCurrency.equals("USD")) {
+            showRatesLabel.setText("1 USD = 110.5 BDT");
+        } else if (selectedCurrency.equals("EUR")) {
+            showRatesLabel.setText("1 EUR = 120.0 BDT");
+        } else if (selectedCurrency.equals("JPY")) {
+            showRatesLabel.setText("1 JPY = 0.75 BDT");
+        } else if (selectedCurrency.equals("INR")) {
+            showRatesLabel.setText("1 INR = 1.3 BDT");
+        } else {
+            showRatesLabel.setText("Unknown currency.");
         }
     }
 
